0d1913089618d8805ec9b8cf926a1ba88a1a0ae1
Ademir Giraldelli <16231827+aeagiraldelli@users.noreply.github.com>
2023-07-27 11:07:20 -0300

    finished project 'guess the number'

---
+++ stage-04/projects/guess-the-number/script.js
@@ -0,0 +1,50 @@
+let btnGuess = document.getElementById('btn-guess');
+let inputGuess = document.getElementById('input-guess');
+let resultMessage = document.getElementById('result-message');
+let app = document.getElementById('app');
+let result = document.getElementById('result');
+let btnTryAgain = document.getElementById('btn-try-again');
+let errorMessage = document.getElementById('error-message');
+
+function genRandomNumber() {
+  return Math.round(Math.random() * 10);
+}
+
+let timer = null;
+let attempts = 0;
+let secretNumber = genRandomNumber();
+
+errorMessage.classList.add('hide');
+
+btnGuess.onclick = () => {
+  let userGuess = Number(inputGuess.value);
+  attempts++;
+
+  if (userGuess === secretNumber) {
+    resultMessage.innerHTML = `Acertou em ${attempts} tentativas.`;
+    app.classList.add('hide');
+    result.classList.remove('hide');
+  } else {
+    errorMessage.classList.remove('hide');
+    if (timer !== null) {
+      clearTimeout(timer);
+      timer = setTimeout(() => {
+        errorMessage.classList.add('hide');
+      }, 2000);
+    } else {
+      timer = setTimeout(() => {
+        errorMessage.classList.add('hide');
+      }, 2000);
+    }
+  }
+};
+
+btnTryAgain.onclick = () => {
+  attempts = 0;
+  secretNumber = genRandomNumber();
+  inputGuess.value = 0;
+
+  result.classList.add('hide');
+  errorMessage.classList.add('hide');
+  app.classList.remove('hide');
+};
