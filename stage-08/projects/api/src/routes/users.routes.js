5f3aa8ab8a77f030e566545411d91650e80afabf
Ademir Giraldelli <16231827+aeagiraldelli@users.noreply.github.com>
2023-08-20 20:11:59 -0300

    added user routes.

---
+++ stage-08/projects/api/src/routes/users.routes.js
@@ -0,0 +1,13 @@
+import { Router } from 'express';
+import { UserController } from '../controllers/UserController.js';
+
+export const userRoutes = Router();
+
+const userController = new UserController();
+
+userRoutes.get('/', (req, res) => {
+  const { page, limit } = req.query;
+  res.send(`<h1>Page requested: ${page}</h1><h2>Limit: ${limit}</h2>`);
+});
+
+userRoutes.post('/', userController.create);
