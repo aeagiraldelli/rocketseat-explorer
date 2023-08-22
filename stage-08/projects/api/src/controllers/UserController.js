897f6092ac463e8cded0dbd23e802ddbcdd1cbf6
Ademir Giraldelli <16231827+aeagiraldelli@users.noreply.github.com>
2023-08-21 21:43:24 -0300

    user sign up.

--- stage-08/projects/api/src/controllers/UserController.js
+++ stage-08/projects/api/src/controllers/UserController.js
@@ -1,13 +1,41 @@
 import { AppError } from '../utils/AppError.js';
+import { databaseConnection } from '../database/sqlite/database.js';
 
 export class UserController {
-  create(req, res) {
+  async create(req, res) {
     const { name, email, password } = req.body;
 
     if (!name) {
-      throw new AppError('parameter "name" is required.');
+      throw new AppError('Nome é obrigatório.');
     }
 
-    res.status(201).json({ name, email });
+    if (!password) {
+      throw new AppError('Senha é obrigatório.');
+    }
+
+    if (!email) {
+      throw new AppError('Email é obrigatório.');
+    }
+
+    const db = await databaseConnection();
+    const query = 'SELECT * FROM users WHERE email = (?)';
+    const userExists = await db.get(query, [email]);
+
+    if (userExists) {
+      throw new AppError('Este email já está sendo utilizado!');
+    }
+
+    const cmd = `INSERT INTO users 
+                 (name, email, password)
+                 VALUES
+                 (?, ?, ?)`;
+
+    try {
+      await db.run(cmd, [name, email, password]);
+    } catch (error) {
+      throw new AppError('Erro ao cadastrar novo usuário.', 500);
+    }
+
+    return res.status(201).json({});
   }
 }
