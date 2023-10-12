f08a267fab680cac0042b584050c2f499551f8a4
Ademir Giraldelli <16231827+aeagiraldelli@users.noreply.github.com>
2023-10-12 12:46:36 -0300

    added pm2

--- stage-08/projects/api/ecosystem.config.cjs
+++ stage-08/projects/api/ecosystem.config.cjs
@@ -1,7 +1,7 @@
-export default {
+module.exports = {
   apps: [{
     name: "app",
-    script: "./app.js",
+    script: "./src/server.js",
     instances: "max",
     env: {
       NODE_ENV: "development",
